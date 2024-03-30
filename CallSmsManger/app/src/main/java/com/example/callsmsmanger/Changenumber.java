3446cf1685ae0c363cd1e302bbbcf6aac89e2e6c
Mohmmadhosein Abdollahi <153551779+Mohiabd99@users.noreply.github.com>
2024-03-30 17:22:41 +0100

    Functionality is completed (my-way)

--- CallSmsManger/app/src/main/java/com/example/callsmsmanger/Changenumber.java
+++ CallSmsManger/app/src/main/java/com/example/callsmsmanger/Changenumber.java
@@ -1,6 +1,7 @@
 package com.example.callsmsmanger;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -91,6 +92,13 @@ public class Changenumber extends AppCompatActivity {
             Intent intent = new Intent(Changenumber.this, Makeintcall.class);
             startActivity(intent);
         });
+        Button info = findViewById(R.id.info);
+        info.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://en.wikipedia.org/wiki/List_of_international_call_prefixes"));
+            startActivity(intent);
+        });
+
 
     }
 }
